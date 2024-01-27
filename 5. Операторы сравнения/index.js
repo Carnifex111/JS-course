11effbcdb26e870bc4cc392db9e66c3d663e617b
Shirov Ivan <117617823+Carnifex111@users.noreply.github.com>
2024-01-27 15:12:05 +0500

    .

---
+++ 5. Операторы сравнения/index.js
@@ -0,0 +1,7 @@
+console.log(5 > 4); // true
+console.log("ананас" > "яблоко"); // false
+console.log("2" > "12"); // true, потому что сравниваются строки, а не числа
+console.log(undefined == null); // true
+console.log(undefined === null); // false
+console.log(null == "\n0\n"); // false
+console.log(null === +"\n0\n"); // false
